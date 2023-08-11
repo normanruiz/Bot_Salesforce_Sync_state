54a03c4f93bb18e59a7771711d412ac2dca6e8ab
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-08-10 23:29:09 -0300

    Agrego en el paquete de modelos la clase Terminal.

---
+++ Modelo/Terminal.py
@@ -0,0 +1,20 @@
+class Terminal:
+    def __init__(self, numero=None, estado=None ):
+        self._numero = numero
+        self._estado = estado
+
+    @property
+    def numero(self):
+        return self._numero
+
+    @numero.setter
+    def numero(self, numero):
+        self._numero = numero
+
+    @property
+    def estado(self):
+        return self._estado
+
+    @estado.setter
+    def estado(self, estado):
+        self._estado = estado
